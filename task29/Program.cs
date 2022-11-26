0a31bd284a0714a26fd86ea1997635545ccfa654
Skozhzhan <106809865+Skozhzhan@users.noreply.github.com>
2022-11-26 16:48:05 +0300

    задать и вывести массив из 8 элементов

---
+++ task29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] numbers(int a)
+{
+    int[] arr = new int[8];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-5, 7);
+    }
+    return arr;
+}
+
+string A(int[] a)
+{
+    string result = string.Empty;
+    for (int i = 0; i < a.Length; i++)
+    {
+        result += ($"{a[i]} ");
+    }
+    return result;
+}
+Console.WriteLine(A(numbers(8)));
