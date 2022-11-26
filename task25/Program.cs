af89addd7d7f274afd2bbf746348bacea68e71d2
Skozhzhan <106809865+Skozhzhan@users.noreply.github.com>
2022-11-26 16:32:12 +0300

    возведение А в степень В

---
+++ task25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение степени: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int Method4(int a, int b)
+{
+    // возведение в степень
+    int result = a;
+    for (int i = 1; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+int res = Method4(a, b);
+Console.WriteLine(res);
